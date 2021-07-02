7b537ec5b7154f20a26473c1e9fd6bb7e171a601
Deepak Meena <deepmeena70@gmail.com>
2021-07-02 15:36:14 +0530

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+// import './style.css';
+import "@fortawesome/fontawesome-free/js/brands.min";
+import "@fortawesome/fontawesome-free/js/solid.min";
+import "@fortawesome/fontawesome-free/js/regular.min";
+import "@fortawesome/fontawesome-free/js/fontawesome.min";
+import "../scss/style.scss";
+import "./sphereWireframe";
+// import "./threeText";
+// import "./skinnedMesh";
